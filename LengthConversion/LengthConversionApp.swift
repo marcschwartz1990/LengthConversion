a25faae519c456e65d5bfae70129c0447735a7c2
Marc Schwartz <marcschwartz1990@gmail.com>
2022-07-30 23:56:36 -0400

    Initial Commit

---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Marc-Developer on 7/30/22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
